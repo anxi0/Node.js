499c6e8acec96859c9c7eefaaca1a3e9e1dfbc00
anxi0 <jjw01040@gmail.com>
2020-12-18 17:45:42 +0900

    75p!

--- 2.js_basics/1.es5+.js
+++ 2.js_basics/1.es5+.js
@@ -52,6 +52,64 @@ const not2 = (x) => !x;
 //this binding
 //if you use arrow function in forEach or something, you can use .this directly. But just function(){}... should bind this to other variables
 
-
 //TODO:72p
-const {a,b} = 
+//Destructuring assignment : such a beautiful Grammer
+var hello = {
+  hi: 'a',
+  hai: 'b',
+};
+const { hai, hi } = hello;
+console.log(hi, hai); //freaking cool
+//at array
+const arr = ['a', 1, {}, true];
+const [string, number, , bool] = arr;
+// no object
+
+//TODO: 73p
+//Class
+var Human = function (type) {
+  this.type = type || 'human';
+};
+
+Human.isHuman = function (human) {
+  return human instanceof Human;
+};
+
+Human.prototype.breathe = function () {
+  alert('haaaaam');
+};
+var Zero = function (type, firstName, lastName) {
+  Human.apply(this, arguments);
+  this.firstName = firstName;
+  this.lastName = lastName;
+};
+Zero.prototype = Obejct.create(Human.prototype);
+Zero.prototype.constructor = Zero;
+Zero.prototype.sayName = function () {
+  alert(this.firstName + ' ' + this.lastName);
+};
+var oldZero = new Zero('Human', 'Zero', 'Cho');
+
+console.log(Human.isHuman());
+//Just pass'em
+
+//TODO: 75p
+//promise
+const condition = true;
+const promise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('success');
+  } else {
+    reject('fail');
+  }
+});
+promise
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+  .finally(() => {
+    console.log('Unconditionally');
+  });
